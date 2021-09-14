aa69de7ce6c568bd3c06b777f89eb2686d78998e
John Young <87405768+jyoung0613@users.noreply.github.com>
2021-09-14 16:22:02 -0600

    removed previous lines

--- app.js
+++ app.js
@@ -1,17 +1,3 @@
 //const profileDataArgs = process.argv.slice(2);
-
 const generatePage = () => 'Name: Jane, Github: janehub';
 
-//const printProfileData = profileDataArr => {
-//    This...
-//   for (let i = 0; i < profileDataArr.length; i += 1) {
-//     console.log(profileDataArr[i]);
-//   
-//  
-//    console.log('================');
-//  
-//    // Is the same as this...
-//    profileDataArr.forEach(profileItem => console.log(profileItem));
-//  };
-
-//printProfileData(profileDataArgs);
